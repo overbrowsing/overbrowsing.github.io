20a878590c3542d5ca83010bada6858fd92bbc89
printer_scanner <abbey@fictivekin.com>
2023-11-07 11:40:40 +0100

    fix: test new yaml workflow

--- scripts/fetchCarbon.js
+++ scripts/fetchCarbon.js
@@ -1,5 +1,8 @@
 /********** Website Carbon **********/
-fetch('/api/carbon') // Assuming your Node.js server is running on the same domain
+// This only runs on build 
+const dataPath = '/cache/carbon-data.json'; // Adjust the path to match your file location
+
+fetch(dataPath)
   .then(response => {
     if (!response.ok) {
       throw new Error(`Fetch error: ${response.status} - ${response.statusText}`);
@@ -7,6 +10,7 @@ fetch('/api/carbon') // Assuming your Node.js server is running on the same doma
     return response.json();
   })
   .then(data => {
+    // Now that you have fetched the JSON data, you can use it in your code
     const resultsElement = document.getElementById('carbon');
     const renewableGrams = data.statistics.co2.renewable.grams;
     resultsElement.innerHTML = `This is a low-consumption site that uses just ${renewableGrams.toFixed(2)}g CO2e and runs on clean energy.`;
--- scripts/server.cjs
+++ scripts/server.cjs
@@ -13,18 +13,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.get('/api/carbon', async (req, res) => {
-  try {
-    const response = await fetch('https://api.websitecarbon.com/site?url=https://overbrowsing.com');
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
-
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
