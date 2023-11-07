dcdad5de6026a7440782422db3b57a403548e72c
printer_scanner <abbey@fictivekin.com>
2023-11-07 11:22:40 +0100

    fix: update carbon calculator to prevent CORS issue

--- scripts/fetchCarbon.js
+++ scripts/fetchCarbon.js
@@ -1,8 +1,16 @@
 /********** Website Carbon **********/
-fetch(`https://api.websitecarbon.com/site?url=https://overbrowsing.com`)
-  .then(response => response.json())
+fetch('/api/carbon') // Assuming your Node.js server is running on the same domain
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Fetch error: ${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  })
   .then(data => {
     const resultsElement = document.getElementById('carbon');
     const renewableGrams = data.statistics.co2.renewable.grams;
     resultsElement.innerHTML = `This is a low-consumption site that uses just ${renewableGrams.toFixed(2)}g CO2e and runs on clean energy.`;
+  })
+  .catch(error => {
+    console.error('Error:', error);
   });
--- scripts/server.cjs
+++ scripts/server.cjs
@@ -7,10 +7,23 @@ const port = 3000;
 // Serve static files from the root directory (CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, '../')));
 
+
 // Define a route to serve the index.html from the root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+app.get('/api/carbon', async (req, res) => {
+  try {
+    const response = await fetch('https://api.websitecarbon.com/site?url=https://overbrowsing.com');
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
