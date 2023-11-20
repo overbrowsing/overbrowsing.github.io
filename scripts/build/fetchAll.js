500d6fab736120b0a516e7a48d64471a18a98c58
printer_scanner <abbey@fictivekin.com>
2023-11-20 13:31:31 +0100

    fix stop workflow; make fetchAll more efficient

--- scripts/build/fetchAll.js
+++ scripts/build/fetchAll.js
@@ -2,114 +2,101 @@ import { promises as fsp, existsSync } from 'fs';
 import fetch from 'node-fetch';
 import sharp from 'sharp';
 import path from 'path';
-import fs from 'fs'
 
 const channelCategories = [
-    { name: "design-tool-rzfxmnmei8g", category: "Design Tools" },
-    { name: "founding-principles", category: "Founding Principals" },
-    // Add more channel names and categories as needed
+  { name: "design-tool-rzfxmnmei8g", category: "Design Tools" },
+  { name: "founding-principles", category: "Founding Principals" },
+  // Add more channel names and categories as needed
 ];
 
 async function downloadImage(url, filePath) {
-    const response = await fetch(url);
-    if (response.status === 200) {
-        const imageArrayBuffer = await response.arrayBuffer();
-        const imageBuffer = Buffer.from(imageArrayBuffer);
-        await fsp.writeFile(filePath, imageBuffer);
-    }
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const imageArrayBuffer = await response.arrayBuffer();
+    const imageBuffer = Buffer.from(imageArrayBuffer);
+    await fsp.writeFile(filePath, imageBuffer);
+  }
 }
 
-// Compression algorithm using sharp 
-// https://sharp.pixelplumbing.com/
-// Image output is currently 87KB, this can be improved. 
 async function processAndOptimizeImage(inputFilePath, outputFilePath) {
-    await sharp(inputFilePath)
-        .resize(350) // Adjust dimensions as needed
-        .toFormat('png')
-        .png({
-        dither: .9, // Enable dithering
-        palette: true, // Create a color palette for dithering
-        colors: 3,
-        quality: 5,  // Adjust quality level as needed
-        })
-        .toFile(outputFilePath);
+  await sharp(inputFilePath)
+    .resize(350) // Adjust dimensions as needed
+    .toFormat('png')
+    .png({
+      dither: 0.9,
+      palette: true,
+      colors: 3,
+      quality: 5,
+    })
+    .toFile(outputFilePath);
+}
+
+async function processChannel(channel) {
+  const data = await fetch(`https://api.are.na/v2/channels/${channel.name}?per=200`).then(resp => resp.json());
+  const category = channel.category;
+
+  const imagesDir = 'cache/images';
+  await fsp.mkdir(imagesDir, { recursive: true });
+
+  const mergedJson = [];
+
+  await Promise.all(data.contents.map(async element => {
+    const reducedData = {
+      title: element.title,
+      content: element.content,
+      contents: element.contents,
+      id: element.id,
+      image: element.image,
+      date: element.updated_at,
+      category,
+    };
+
+    if (element.source) {
+      reducedData.source = {
+        url: element.source.url,
+        title: element.source.title,
+        provider: element.source.provider,
+      };
     }
 
+    if (element.image) {
+      const inputFilePath = path.join(imagesDir, `${element.id}.png`);
+      const tempOutputFilePath = path.join('cache/temp', `${element.id}.png`);
 
-// This function checks for an image cache and if found deletes it
-// Downloads the images and runs the compresssion algorithm and saves the images to cache/images/
-// Downloads sorts and merges the JSON file and saves it to cache/data.json
+      await downloadImage(element.image.display.url, inputFilePath);
+      await processAndOptimizeImage(inputFilePath, tempOutputFilePath);
+
+      await fsp.rename(tempOutputFilePath, inputFilePath);
+    }
+
+    mergedJson.push(reducedData);
+  }));
+
+  return mergedJson;
+}
+
 async function compressAndSave() {
-    try {
-        const imagesDir = 'cache/images';
-        const tempDir = 'cache/temp';
-
-        if (existsSync(imagesDir)) {
-            await fsp.rm(imagesDir, { recursive: true });
-        }
-
-        await fsp.mkdir(imagesDir, { recursive: true });
-        await fsp.mkdir(tempDir, { recursive: true });
-
-        const data = await Promise.all(
-            channelCategories.map((channel) =>
-                fetch(`https://api.are.na/v2/channels/${channel.name}?per=200`).then((resp) => resp.json())
-            )
-        );
-
-        const mergedJson = [];
-
-        for (let index = 0; index < data.length; index++) {
-            const category = channelCategories[index].category;
-
-            const channelData = data[index].contents;
-
-            for (const element of channelData) {
-
-                const reducedData = {
-                    title: element.title,
-                    content: element.content,
-                    contents: element.contents,
-                    id: element.id,
-                    image: element.image,
-                    date: element.updated_at,
-                    category: category,
-                };
-
-                if (element.source) {
-                    reducedData.source = {
-                        url: element.source.url,
-                        title: element.source.title,
-                        provider: element.source.provider,
-                    };
-                }
-
-        
-                element.category = category;
-
-                if (element.image) {
-                    const inputFilePath = path.join(imagesDir, `${element.id}.png`);
-                    const tempOutputFilePath = path.join(tempDir, `${element.id}.png`);
-
-                    await downloadImage(element.image.display.url, inputFilePath);
-                    await processAndOptimizeImage(inputFilePath, tempOutputFilePath);
-
-                    await fsp.rename(tempOutputFilePath, inputFilePath);
-                }
-            mergedJson.push(reducedData);
-            }
-        }
-        mergedJson.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-        await fsp.writeFile("cache/data.json", JSON.stringify(mergedJson));
-        
-        // Delete the temporary folder
-        await fsp.rm(tempDir, { recursive: true });
-        console.log("Images Downloaded")
-    } catch (err) {
-        console.error('Error:', err);
+  try {
+    if (existsSync('cache/images')) {
+      await fsp.rm('cache/images', { recursive: true });
     }
-};
+
+    await fsp.mkdir('cache/temp', { recursive: true });
+
+    const data = await Promise.all(channelCategories.map(processChannel));
+
+    const mergedJson = data.flat().sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    await fsp.writeFile("cache/data.json", JSON.stringify(mergedJson));
+    
+    // Delete the temporary folder
+    await fsp.rm('cache/temp', { recursive: true });
+
+    console.log("Images Downloaded");
+  } catch (err) {
+    console.error('Error:', err);
+  }
+}
 
 // FETCH AQI
 async function fetchAqiAndSave() {
@@ -167,7 +154,7 @@ function calculateBackgroundColor(aqiValue) {
 // Save data to a JSON file
 function saveToJson(data) {
   const json = JSON.stringify(data, null, 2);
-  fs.writeFileSync('cache/aqi-data.json', json, 'utf8');
+  fsp.writeFile('cache/aqi-data.json', json, 'utf8');
 }
 
 
