1c38fbcf98baf686f4e85cd87423a4032b0016e1
HEADLESS HORSE <github-login@headless.horse>
2024-12-23 00:32:07 +0000

    Update 'menu' script

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,8 +109,8 @@ let lastScrollY = 0, activeParent = null;
 
 const updateHeader = () => {
   const scrollY = window.scrollY;
-  header.style.background = scrollY >= 300 ? 'var(--color-primary)' : '';
-  header.style.top = scrollY < lastScrollY || scrollY < 250 ? '0' : '-7em';
+  header.style.background = scrollY >= 150 ? 'var(--color-primary)' : '';
+  header.style.top = scrollY < lastScrollY || scrollY < 100 ? '0' : '-7em';
 
   if (scrollY > lastScrollY && activeParent) {
     const nextSibling = activeParent?.nextElementSibling;
