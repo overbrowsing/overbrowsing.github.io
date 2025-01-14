aaee94b8345eff4b407bd9bb3dc7bf51719bf322
HEADLESS HORSE <github-login@headless.horse>
2025-01-14 23:13:26 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,7 +110,7 @@ let lastScrollY = 0, activeParent = null;
 const updateHeader = () => {
   const scrollY = window.scrollY;
   header.style.background = scrollY >= 80 ? 'var(--color-primary)' : '';
-  header.style.top = scrollY < lastScrollY || scrollY < 50 ? '0' : '-6em';
+  header.style.top = scrollY < lastScrollY || scrollY < 50 ? '0' : '-80px';
 
   if (scrollY > lastScrollY && activeParent) {
     const nextSibling = activeParent?.nextElementSibling;
